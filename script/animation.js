19d0a7f73a4b76b3422968dc0338a820bf19c9e2
Ephyekas <80513723+Ephyekas@users.noreply.github.com>
2021-07-21 21:51:21 +0200

    nav

---
+++ script/animation.js
@@ -0,0 +1,9 @@
+
+
+
+let a = document.getElementById("animated-svg");
+
+let b = a.contentDocument;
+
+<svg id="logo1"></svg>
+c = b.getElementById("logo1");
---
+++ vue/vue_navBar.php
@@ -0,0 +1,67 @@
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="../css/nav.css" rel="stylesheet"/>
+    <script src="../script/animation.js"></script>
+    <title>Barre de navigation</title>
+</head>
+<body>
+    <header>
+        <div id="en-tete">
+
+            <div class="en_tete" >
+                <h1 id="titre">YGGDRASIL</h1>
+            </div>
+
+         
+                <div id="bg-svg" class="en_tete">
+                    
+                    <object id ="animated-svg" type="image/svg+xml" data="../images/animation/logo1.svg"></object>
+                </div>
+                    
+     
+
+            <div class="en_tete" >
+                <h1 >Venez découvrir les <span>9 Royaumes!</span></h1>
+            </div>
+
+        </div>
+            
+        <nav>
+            <div id="con-res">
+
+
+                <div class="con_res">
+                    <button id="res"
+                        type="button">Réservation
+                    </button>
+                </div> 
+
+                <div class="con_res"> 
+                    <button id="connexion"
+                        type="button">Connexion
+                    </button>
+                </div>  
+            </div>
+
+            <div id="image">
+                <img src="../images/image/Nav/img_accueil.jpg"  id="fondecran" class="fondecran"/>
+            </div>
+
+
+            <div id="liste">
+                    <!-- <li id="Accueil">Accueil</li> -->
+                    <p id="Reservation" class="navigation"><img id="img_reservation" src="../images/image/Nav/navBar/Réservation.png">Réservation</p>
+                    <p id="Evenement" class="navigation"><img id="img_evenement" src="../images/image/Nav/navBar/evenement.svg">Evènement</p>
+                    <p id="Attraction" class="navigation"><img id="img_attraction" src="../images/image/Nav/navBar/attraction.png">Attraction</p>
+                    <p id="Cours" class="navigation"><img id="img_cours" src="../images/image/Nav/navBar/cours.png">Cours</p>
+                    <p id="Plan" class="navigation"><img id="img_plan" src="../images/image/Nav/navBar/Plan.png">Plan</p>
+                
+            </div>
+        </nav>
+    </header>
+   
+</body>
+</html>
